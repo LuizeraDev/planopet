ad2c4d3485ed60d6f5f3c0c5ff51351549d492a8
Marcos Alexandre Rodrigues de Carvalho <45478002+marcos96x@users.noreply.github.com>
2020-06-10 17:53:58 -0300

    Aplicando responsividade na tela inicial

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, {height: 800, indicators: false});
+
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
 });
