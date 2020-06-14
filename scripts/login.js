40f54c2ab5804b9aa65090806e2feb1b3b8a6b0d
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-06-14 03:42:20 -0300

    funcionalidades e reparos

--- scripts/login.js
+++ scripts/login.js
@@ -2,3 +2,15 @@ $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
 
+ function entrar(){
+    var convenio = document.getElementById("tipoconta1"); 
+    var clinica = document.getElementById("tipoconta2"); 
+
+    if(convenio.checked == true)
+        window.location.href = "../pages/homeConvenio.html";
+    if(clinica.checked == true)
+        window.location.href = "../pages/homeClinica.html";
+    else if(convenio.unchecked && clinica.unchecked)
+        alert("Por favor, escolha um tipo e conta.");
+ }
+
