b0a6ccb165067a95abbf2a59658d4848eb3a9377
Marcos Alexandre Rodrigues de Carvalho <45478002+marcos96x@users.noreply.github.com>
2020-06-14 20:52:46 -0300

    aplicando responsividade em algumas telas

--- scripts/gerenciarPerfilClinica.js
+++ scripts/gerenciarPerfilClinica.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
 
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+
  
 });
 
@@ -13,6 +16,14 @@ function setTela(tela) {
             $("#planosLink").attr('class', 'collection-item transparent black-text');
             $("#opcoesLink").attr('class', 'collection-item transparent black-text');
 
+            $("#mobileDados").attr('class', 'light-green darken-4 white-text ativo');
+            $("#mobilePlanos").attr('class', '');
+            $("#mobileOpcoes").attr('class', '');
+
+            $("#mobileDados").html(`<a class='white-text'>Dados da Clinica</a>`);
+            $("#mobilePlanos").html(`<a>Planos Atendidos</a>`);
+            $("#mobileOpcoes").html(`<a>Opções Avançadas</a>`);
+
             $("#dados").show();
             $("#planos").hide();
             $("#opcoes").hide();
@@ -21,6 +32,14 @@ function setTela(tela) {
             $("#dadosLink").attr('class', 'collection-item transparent black-text');
             $("#planosLink").attr('class', 'collection-item active light-green darken-4');
             $("#opcoesLink").attr('class', 'collection-item transparent black-text');
+
+            $("#mobileDados").attr('class', '');
+            $("#mobilePlanos").attr('class', 'light-green darken-4 white-text ativo');
+            $("#mobileOpcoes").attr('class', '');
+
+            $("#mobileDados").html(`<a>Dados da Clinica</a>`);
+            $("#mobilePlanos").html(`<a class='white-text'>Planos Atendidos</a>`);
+            $("#mobileOpcoes").html(`<a>Opções Avançadas</a>`);
 
             $("#dados").hide();
             $("#planos").show();
@@ -31,6 +50,14 @@ function setTela(tela) {
             $("#planosLink").attr('class', 'collection-item transparent black-text');
             $("#opcoesLink").attr('class', 'collection-item active light-green darken-4');
 
+            $("#mobileDados").attr('class', '');
+            $("#mobilePlanos").attr('class', '');
+            $("#mobileOpcoes").attr('class', 'light-green darken-4 white-text ativo');
+
+            $("#mobileDados").html(`<a>Dados da Clinica</a>`);
+            $("#mobilePlanos").html(`<a>Planos Atendidos</a>`);
+            $("#mobileOpcoes").html(`<a class='white-text'>Opções Avançadas</a>`);
+
             $("#dados").hide();
             $("#planos").hide();
             $("#opcoes").show();
@@ -39,6 +66,14 @@ function setTela(tela) {
             $("#dadosLink").attr('class', 'collection-item active  light-green darken-4');
             $("#planosLink").attr('class', 'collection-item transparent black-text');
             $("#opcoesLink").attr('class', 'collection-item transparent black-text');
+
+            $("#mobileDados").attr('class', 'light-green darken-4 white-text ativo');
+            $("#mobilePlanos").attr('class', '');
+            $("#mobileOpcoes").attr('class', '');
+
+            $("#mobileDados").html(`<a class='white-text'>Dados da Clinica</a>`);
+            $("#mobilePlanos").html(`<a>Planos Atendidos</a>`);
+            $("#mobileOpcoes").html(`<a>Opções Avançadas</a>`);
             
             $("#dados").show();
             $("#planos").hide();
